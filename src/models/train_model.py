d105ba231482272d3096953e186b8e344fb03688
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-01-10 22:08:18 +0530

    tf-exp-01

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,7 @@
 import datetime
 import pandas as pd
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 
@@ -21,8 +22,6 @@ model_dl = tf.keras.Sequential([
         tf.keras.layers.Dense(256, activation='relu'),
         tf.keras.layers.Dense(512, activation='relu'),
         tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(1024, activation='relu'),
-        tf.keras.layers.Dense(1024, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid', kernel_regularizer=tf.keras.regularizers.l2(l2=0.1))
     ])
 
@@ -34,5 +33,13 @@ log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 cb_list = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20),  tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)]
 
 history = model_dl.fit(x_train, y_train,
-                   epochs=100,
+                   epochs=60,
                    validation_split=0.3, callbacks=cb_list)
+
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.savefig("accuracy.png")
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.savfig("loss.png")
