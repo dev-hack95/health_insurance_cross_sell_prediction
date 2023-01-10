91a6bc4aa482602272927f190e0385c143c5ce1f
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-01-10 15:11:57 +0530

    tf-exp-01

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,9 +1,6 @@
+import datetime
 import pandas as pd
 import tensorflow as tf
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 
@@ -24,6 +21,8 @@ model_dl = tf.keras.Sequential([
         tf.keras.layers.Dense(256, activation='relu'),
         tf.keras.layers.Dense(512, activation='relu'),
         tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dense(1024, activation='relu'),
         tf.keras.layers.Dense(1, activation='sigmoid', kernel_regularizer=tf.keras.regularizers.l2(l2=0.1))
     ])
 
@@ -31,17 +30,9 @@ model_dl.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                  loss=tf.keras.losses.BinaryCrossentropy(),
                  metrics=['accuracy'])
 
-cb_list = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)]
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+cb_list = [tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20),  tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)]
 
 history = model_dl.fit(x_train, y_train,
                    epochs=100,
                    validation_split=0.3, callbacks=cb_list)
-
-
-
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.savefig("accuracy.png")
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.savefig("loss.png")
