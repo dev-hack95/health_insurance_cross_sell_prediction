926544702d6fef48f793c91655df9a15d3e092d1
dev-hack95 <saiprasadtoshatwad@gmail.com>
2023-03-16 09:42:06 +0530

    train

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,5 @@
 import pickle
+import json
 import pandas as pd
 import seaborn as sns
 import numpy as np
@@ -6,47 +7,34 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix
 from imblearn.over_sampling import SMOTE
 
 df = pd.read_csv("./data/raw/train_processed.csv")
-df = df.sample(n=50000)
+df = df.sample(n=10000)
 x = df.iloc[:, :-1]
 y = df.iloc[:, -1]
 
 smote = SMOTE(random_state=42)
 x, y = smote.fit_resample(x, y)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
 
-
-
-
-clf = BaggingClassifier(
-    estimator=DecisionTreeClassifier(criterion='gini',
-      max_depth=50,
-      min_samples_split=3
-      ),
-    n_estimators=300,
-    bootstrap_features=True,
-    oob_score=True,
-    max_features=1.0
-    )
-
+clf = BaggingClassifier(estimator=DecisionTreeClassifier(), n_estimators=100, oob_score=True)
 
 
 clf.fit(x_train , y_train)
 y_pred = clf.predict(x_test)
-acc = clf.score(x_test, y_test)
-print(acc)
+accuracy = clf.score(x_test, y_test)
+print(accuracy)
 
-#metrics = """
-#Accuracy: {:10.4f}
-#![Confusion Matrix](plot.png)
-#""".format(acc)
-#with open("metrics.txt", "w") as outfile:
-#    outfile.write(metrics)
-#conf_matrix = confusion_matrix(y_test, y_pred)
-#disp = sns.heatmap(conf_matrix/np.sum(conf_matrix), annot=True, cmap='Blues', fmt='.2%')
-#plt.savefig("plot.png")
+acc = accuracy * 100
+
+tn, fp, fn, tp = confusion_matrix(y, y_pred).ravel()
+specificity = tn / (tn + fp)
+sensitivity = tp / (tp + fn)
+
+with open('metrics.json') as outfile:
+    json.dump({ "accuracy": acc,  "specificity": specificity, "sensitivity": sensitivity })
 
 pickle.dump(clf, open('./artifacts/model.pkl', 'wb'))
